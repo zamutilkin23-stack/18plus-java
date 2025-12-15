8cb3408d3b72f901c8de1d038bb144313910b365
zamutilkin23-stack <zamutilkin23@gmail.com>
2025-12-15 12:31:38 +0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
+import sys
+print(f"[Чаки] Запущено с Python: {sys.version}")
+print(f"[Чаки] Версия: {sys.version_info}")
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+    print("[Чаки] Сервер запущен на порту 7000")
     app.run(host='0.0.0.0', port=7000, debug=False)
